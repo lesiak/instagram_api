e9e239f9a16ccb27ea54c65732581b2060c42454
lesiak <lesiak@outlook.com>
2019-03-29 13:36:08 +0100

    Initial commit

---
+++ test/instagram_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:instagram_api/instagram_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
