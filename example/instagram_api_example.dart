b24f3d6fe45a35bf9318257408b7ab559a0ae3d7
lesiak <lesiak@outlook.com>
2019-04-03 17:20:45 +0200

    Start authentication implementation

--- example/instagram_api_example.dart
+++ example/instagram_api_example.dart
@@ -1,12 +1,9 @@
 import 'package:instagram_api/instagram_api.dart';
 
 main() async {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-  
   InstagramScrapper igScrapper = InstagramScrapper(["_ulalla"]);
-  await igScrapper.authenticate_as_guest();
-  print("Logged in as aguest");
-  await igScrapper.scrape();
+  await igScrapper.authenticateWithLogin();
+  //print("Logged in as aguest");
+  //await igScrapper.scrape();
   igScrapper.close();
 }
--- lib/src/instagram_scrapper.dart
+++ lib/src/instagram_scrapper.dart
@@ -17,7 +17,10 @@ class InstagramScrapper {
 
   Map<String, String> global_headers;
 
-  Future<String> authenticate_as_guest() async {
+  /**
+   *  Authenticate as a guest/non-signed in user
+   */
+  Future<String> authenticateAsGuest() async {
     //"""Authenticate as a guest/non-signed in user"""
     //self.session.headers.update({'Referer': BASE_URL, 'user-agent': STORIES_UA})
     //req = self.session.get(BASE_URL)
@@ -31,7 +34,7 @@ class InstagramScrapper {
    // String csrfToken = await getXsrfToken();
 
     var headers = {HttpHeaders.userAgentHeader: CHROME_WIN_UA};
-    Map<String, dynamic> shared_data = await get_shared_data('', headers);
+    Map<String, dynamic> shared_data = await getSharedData('', headers);
     var rhx_gis = shared_data['rhx_gis'];
 
     print("rhx_gis: $rhx_gis");
@@ -39,17 +42,38 @@ class InstagramScrapper {
     //self.authenticated = True
   }
 
+  authenticateWithLogin() async {
+    String csrfToken = await getXsrfTokenFromBasePage();
+    print(csrfToken);
+    var headers = {
+      HttpHeaders.refererHeader: BASE_URL,
+      HttpHeaders.userAgentHeader: STORIES_UA,
+      'X-CSRFToken': csrfToken
+    };
+
+  }
+
   void scrape() async {
     var headers = {HttpHeaders.userAgentHeader: STORIES_UA};
     for (var userName in _userNames) {
       // Get the user metadata.
-      Map<String, dynamic> shared_data = await get_shared_data(userName, headers);
-      Map<String, dynamic> user = get_user(shared_data);
-      print(user);
+      Map<String, dynamic> shared_data = await getSharedData(userName, headers);
+      Map<String, dynamic> user = getUser(shared_data);
+      //print(user);
+
+      if (user == null) {
+        print('Error getting user details for $userName. Please verify that the user exists.');
+        continue;
+      }
+      else if (user['is_private'] == true) {
+        print('User $userName is private');
+        continue;
+      }
+
     }
   }
 
-  dynamic get_user(Map<String, dynamic> shared_data) {
+  dynamic getUser(Map<String, dynamic> shared_data) {
     Map<String, dynamic> user;
     try {
       user = shared_data['entry_data']['ProfilePage'][0]['graphql']['user'];
@@ -59,15 +83,15 @@ class InstagramScrapper {
     return user;
   }
 
-  Future<String> getXsrfToken() async {
+  Future<String> getXsrfTokenFromBasePage() async {
     var headers = {HttpHeaders.refererHeader: BASE_URL, HttpHeaders.userAgentHeader: STORIES_UA};
     var response = await httpClient.get(BASE_URL, headers: headers);
     var csrfToken = response.getCookie("csrftoken");
     return csrfToken;
   }
 
-  Future<Map<String, dynamic>> get_shared_data(String username, Map<String, String> headers) async {
-    String resp = await get_json(BASE_URL + username, headers);
+  Future<Map<String, dynamic>> getSharedData(String username, Map<String, String> headers) async {
+    String resp = await getJson(BASE_URL + username, headers);
     if (resp != null && resp.contains('_sharedData')) {
       final shared_data = resp.split("window._sharedData = ")[1].split(";</script>")[0];
       Map<String, dynamic> parsedJson = json.decode(shared_data);
@@ -77,12 +101,12 @@ class InstagramScrapper {
     }
   }
 
-  Future<String> get_json(String url, Map<String, String> headers) async {
-    var response = await safe_get(url, headers);
+  Future<String> getJson(String url, Map<String, String> headers) async {
+    var response = await safeGet(url, headers);
     return response?.body;
   }
 
-  Future<IoHttpResponse> safe_get(String url, Map<String, String> headers) {
+  Future<IoHttpResponse> safeGet(String url, Map<String, String> headers) {
     return httpClient.get(url, headers: headers).then((resp) {
       if (resp.statusCode == 404) {
         return null;
@@ -95,8 +119,4 @@ class InstagramScrapper {
     httpClient.close();
   }
 
-  static bool equalsIgnoreCase(String string1, String string2) {
-    return string1?.toLowerCase() == string2?.toLowerCase();
-  }
-  
 }
